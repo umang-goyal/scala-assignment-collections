4fb791f894ba6a2a7a58d88b40409454ceb6547c
umang-goyal <goel97umang@gmail.com>
2020-02-06 13:06:34 +0530

    added program for question 2 of assignment for changing list element according to whether its key is even or odd

--- src/main/scala/com/knoldus/MapCollections.scala
+++ src/main/scala/com/knoldus/MapCollections.scala
@@ -9,6 +9,21 @@ class MapCollections {
     }
   }
 
+  def changeMap (map: Map[Int, List[String]]): Map[Int, List[String]]= {
+
+    val newMap = Map[Int, List[String]]()
+
+    map.foldLeft(newMap){
+      (a:Map[Int, List[String]], elem: (Int, List[String]))=> if (elem._1%2==0){
+        a++ Map(elem._1-> elem._2.map(x=> x.head+"even"))
+      }
+      else {
+        a++ Map(elem._1-> elem._2.map(x=> x.head+"odd"))
+      }
+    }
+  }
+
+
   def last(list: List[Int]):Any = {
     list match {
       case Nil => "empty list"
@@ -23,4 +38,10 @@ object MapCollections extends App{
   val map = Map("suresh"->4, "ramesh"-> 3, "suraj"-> 2)
   println(obj.sumValueofSimilarKey(map, "su"))
   println(obj.last(List(1,2,3,4,5)))
+  val map2 = Map(1 -> List("Sunil", "Laxmi"),
+    2 -> List("Bhavya", "Sangeeta"),
+    3 -> List("Arun", "Sushmita"),
+    4 -> List("Jamwant")
+  )
+  println(obj.changeMap(map2))
 }
